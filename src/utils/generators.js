fbbb301a727d43235bc71813f3130206d11375b1
Suren Atoyan <s.atoyan@apollobytes.com>
2017-07-23 18:49:00 +0400

    Add JSDoc comments

--- src/utils/generators.js
+++ src/utils/generators.js
@@ -1,3 +1,10 @@
+/**
+ * Utils - generators.
+ * Helpful tools ::: generate random id (for client side), ...
+ * @module utils/generators.js
+ * @author @SurenAt93
+ */
+
 const generateRandomId = _ => Date.now() + Math.random();
 
 export { generateRandomId };
