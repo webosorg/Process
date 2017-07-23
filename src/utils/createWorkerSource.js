c7e3a1082fd2856653696975aac4dfa849c87640
Suren Atoyan <s.atoyan@apollobytes.com>
2017-07-23 18:49:31 +0400

    Change makeWorkerSource to CreateWorkerSource

---
+++ src/utils/createWorkerSource.js
@@ -0,0 +1,45 @@
+/**
+ * CreateWorkerSource
+ * For creating dedicated web worker source dynamically.
+ * @module utils/createWorkerSource.js
+ * @author @SurenAt93
+ */
+
+/**
+ * The CreateWorkerSource class
+ */
+
+export default class CreateWorkerSource {
+
+  /**
+   * Set options.
+   * @params {Function} fn - Function, which should run in web worker.
+   * @params {Array} deps - Array of Dependencies for worker env.
+   */
+
+  constructor(fn, deps = []) {
+
+    this.fn = fn;
+
+    this.deps = deps.map(item => `\'${item}\'`).join(',');
+
+  }
+
+  /**
+   * Public method for creating source for worker
+   * @returns {Function}
+   */
+
+  workerSource() {
+    return Function(
+      `
+        importScripts(${this.deps});
+        const fn = ${this.fn};
+        self.onmessage = msg => {
+          const result = fn(msg.data);
+          self.postMessage(result);
+        }
+      `
+    );
+  }
+}
